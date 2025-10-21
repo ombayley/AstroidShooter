51fe97242c72935b1d6b023b71f0333acf90127e
Olly Bayley <ollybayley1@gmail.com>
2025-10-21 08:56:26 +0200

    cleaning and documatation of shot

--- internal/player/shot.go
+++ internal/player/shot.go
@@ -1,10 +1,13 @@
 package player
 
 import (
+	// internal packages
 	"asteroids/internal/config"
+	// external packages
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
+// Shot struct for player shots
 type Shot struct {
 	Position rl.Vector2
 	Speed    rl.Vector2
@@ -12,17 +15,21 @@ type Shot struct {
 	Active   bool
 }
 
+// Draw the shot to screen
 func (s *Shot) Draw() {
 	if s.Active {
 		rl.DrawCircleV(s.Position, s.Radius, rl.Yellow)
 	}
 }
+
+// Update the shot position
 func (s *Shot) Update() {
-	if s.Active {
-		s.Position.X += s.Speed.X
-		s.Position.Y -= s.Speed.Y
-		if s.Position.X < 0 || s.Position.X > config.ScreenWidth || s.Position.Y < 0 || s.Position.Y > config.ScreenHeight {
-			s.Active = false
-		}
+	if !s.Active {
+		return
+	}
+	s.Position.X += s.Speed.X
+	s.Position.Y -= s.Speed.Y
+	if s.Position.X < 0 || s.Position.X > config.ScreenWidth || s.Position.Y < 0 || s.Position.Y > config.ScreenHeight {
+		s.Active = false
 	}
 }
