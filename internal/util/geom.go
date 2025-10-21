f71615dc779f6d120b2120fc05ec7557c1bc859d
Olly Bayley <ollybayley1@gmail.com>
2025-10-21 08:52:20 +0200

    added doc strings

--- internal/util/geom.go
+++ internal/util/geom.go
@@ -1,10 +1,14 @@
 package util
 
 import (
-	rl "github.com/gen2brain/raylib-go/raylib"
+	// built-in
 	"math"
+
+	// external packages
+	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
+// DirectionVector returns a unit vector in the direction of the rotation
 func DirectionVector(rotationDeg float32) rl.Vector2 {
 	r := float64(rotationDeg) * float64(rl.Deg2rad)
 	return rl.Vector2{
@@ -13,6 +17,7 @@ func DirectionVector(rotationDeg float32) rl.Vector2 {
 	}
 }
 
+// WrapPosition wraps the position of an object around the screen
 func WrapPosition(pos *rl.Vector2, objectSize float32, screenW, screenH float32) {
 	if pos.X > screenW+objectSize {
 		pos.X = -objectSize
